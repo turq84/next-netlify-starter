3b0d31835df7a0ec0bb8141376ec921a2d0c49e9
turq84 <kayla.m.gordon@gmail.com>
2021-04-01 14:19:24 +0300

    feat: update graphql query and add User fragments

--- fragments/links.tsx
+++ fragments/links.tsx
@@ -1,9 +1,22 @@
 export const linksFragment = `
   fragment linksFragment on Link {    
-    name
     _id
+    name   
     url
     description
-    archived    
+    archived
+    userId
+  }
+`;
+
+export const userFragment = `
+  fragment userFragment on User {    
+    _id
+    email
+    password
+    passwordReset
+    authenticated
+    role
+    notes
   }
 `;
